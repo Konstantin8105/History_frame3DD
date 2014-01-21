ae3f5562c5dcc67f6aa531230bf874540785695d
hpgavin <hpgavin@0071cf9a-5322-479b-8c99-447f13b1adbf>
2014-01-21 21:15:12 +0000

    apply linear correction to integrated (M/EI) *before* adding in V/GA

--- src/common.h
+++ src/common.h
@@ -5,7 +5,7 @@
  ---------------------------------------------------------------------------
  http://frame3dd.sourceforge.net/
  ---------------------------------------------------------------------------
- Copyright (C) 1992-2013  Henri P. Gavin
+ Copyright (C) 1992-2014  Henri P. Gavin
 
     FRAME3DD is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -33,7 +33,7 @@
 #define FILENMAX 128
 
 #ifndef VERSION
-#define VERSION "20130318"
+#define VERSION "20140121"
 #endif
 
 #ifndef PI
